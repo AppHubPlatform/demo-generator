10ec3786726e673da3bd237f371d3976ca09b982
Ben Edelstein <ben.edelstein93@gmail.com>
2025-10-11 14:17:19 -0400

    make into a nextjs app

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['@browserbasehq/stagehand']
+  }
+};
+
+export default nextConfig;
