94e3176cd89c37bb532f0a3ae42cc5e64f1e690d
Ben Edelstein <ben.edelstein93@gmail.com>
2025-10-11 22:27:23 -0400

    fix pino error

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['@browserbasehq/stagehand']
+  serverExternalPackages: ['@browserbasehq/stagehand'],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Ignore pino-pretty in production builds
+      config.externals.push('pino-pretty');
+    }
+    return config;
+  }
 };
 
 export default nextConfig;
